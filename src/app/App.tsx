ed765c4b1a4183c756e5cd95e609e57ef6fc89b7
Artyom Shendik <samok99@bk.ru>
2025-08-10 22:00:50 +0300

    feat: minor changes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,6 @@ import { QueryProvider } from './providers/QueryProvider';
 import { AntdProvider } from './providers/AntdProvider';
 import { RouterProvider } from './providers/RouterProvider';
 import 'antd/dist/reset.css';
-import './App.css';
 
 function App() {
   return (
