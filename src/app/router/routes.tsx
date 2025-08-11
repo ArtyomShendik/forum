43d61ceb89cde9d784c8622f690ab80009093eb2
Artyom Shendik <samok99@bk.ru>
2025-08-11 22:11:03 +0300

    feat: update routes

--- src/app/router/routes.tsx
+++ src/app/router/routes.tsx
@@ -1,13 +1,30 @@
 import { Home } from '../../pages/Home/Home';
 import { Profile } from '../../pages/Profile/Profile';
+import { MainLayout } from '../../components/layout/MainLayout';
+import { SinglePost } from '../../pages/SinglePost/SinglePost';
+import { Favorites } from '../../pages/Favorites/Favorites';
 
 export const routes = [
   {
     path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/profile',
-    element: <Profile />,
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+      {
+        path: 'favorites',
+        element: <Favorites />,
+      },
+      {
+        path: 'posts/:id',
+        element: <SinglePost />,
+      },
+    ],
   },
 ];
